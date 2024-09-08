ae0313a59dec3615a2e3ca4be02e778c8065a304
Tobias <wennertobias@gmail.com>
2024-09-08 13:35:16 +0200

    Initial Commit

--- Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65df622a0a05d002e6b54a6fe9f28643ce83f441")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
